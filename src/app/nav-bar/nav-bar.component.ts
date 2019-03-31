40140c3d80d901dc5b39dee58ea670a9cf4bcca2
DevCamilo <alondres15@gmail.com>
2019-03-31 15:50:05 -0500

    :sparkles: finish nav-bar :sparkles:

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    $(document).ready(function(){
+      $('.sidenav').sidenav();
+    });
+  }
+
+}
