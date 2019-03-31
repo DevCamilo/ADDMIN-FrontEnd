8c005b4a85d120085ebdd29494e0d916d9cfe44b
DevCamilo <alondres15@gmail.com>
2019-03-31 18:00:09 -0500

    :door: more home page :door:

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var M:any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+
+  test(){
+    var toastHTML = '<span>I am toast content</span><button class="btn-flat toast-action">Undo</button>';
+    M.toast({html: toastHTML});
+  }
+
 
   ngOnInit() {
   }
